8dac4289c338b723c90c2f04063d18f5345d71b5
Mark Shoun <mark.shoun@gmail.com>
2018-02-01 21:39:26 -0500

    Add a few more words to the sample dictionary

--- src/test/scala/WordListTest.scala
+++ src/test/scala/WordListTest.scala
@@ -3,9 +3,9 @@ import org.scalatest.FlatSpec
 class WordListTest extends FlatSpec {
   "Word lists" should "scrub bad words" in {
     val wordList = WordScoreApp.loadWordList("src/test/resources/sample_dict.txt")
-    assert(wordList.size == 3)
+    assert(wordList.size == 6)
     assert(wordList(0).spelling == "ate")
     assert(wordList(1).spelling == "eat")
-    assert(wordList(2).spelling == "tea")
+    assert(wordList(5).spelling == "ta")
   }
 }
