2b9b6295f0445b2599da9c2391c78aad50362226
Mark Shoun <mark.shoun@gmail.com>
2018-02-02 16:11:57 -0500

    Add option for Boggle-based scoring

--- src/main/scala/Word.scala
+++ src/main/scala/Word.scala
@@ -1,11 +1,37 @@
-class Word(val spelling: String, val residue: List[Char]) {
-  def this(spelling: String) = this(spelling, spelling.toList.sorted)
+object Points {
+  def compute(word: String, method: Symbol): Int = {
+    method match {
+      case 'boggle => computeBogglePoints(word)
+      case _ => 1
+    }
+  }
 
-  val points = 1
+  def computeBogglePoints(word: String): Int = {
+    word.length match {
+      case x if 0 to 2 contains x => 0
+      case 3 => 1
+      case 4 => 1
+      case 5 => 2
+      case 6 => 3
+      case 7 => 5
+      case _ => 11
+    }
+  }
+}
+
+
+class Word(val spelling: String, val points: Int, val residue: List[Char]) {
+  def this(spelling: String) = {
+    this(spelling, 1, spelling.toList.sorted)
+  }
+
+  def this(spelling: String, pointsMethod: Symbol) = {
+    this(spelling, Points.compute(spelling, pointsMethod), spelling.toList.sorted)
+  }
 
   def matchLetter(letter: Char): Word = {
     if (letter == residue.head) {
-      new Word(spelling, residue.tail)
+      new Word(spelling, points, residue.tail)
     } else {
       this
     }
--- src/main/scala/WordScoreApp.scala
+++ src/main/scala/WordScoreApp.scala
@@ -2,7 +2,11 @@ import scala.io.Source
 
 object WordScoreApp {
   type OptionMap = Map[Symbol, Any]
-  val defaultOptions: OptionMap = Map('maxSize -> 7, 'showWords -> false)
+  val defaultOptions: OptionMap = Map(
+    'maxSize -> 7,
+    'showWords -> false,
+    'pointsMethod -> 'default
+  )
 
   def main(args: Array[String]) = {
     val argList = args.toList
@@ -12,12 +16,16 @@ object WordScoreApp {
       System.exit(1)
     }
 
-    val wordList = loadWordList(options('dictFile).asInstanceOf[String])
-    val (bestSet, bestCount) = BestSet.findBestSet(wordList, options('maxSize).asInstanceOf[Int])
+    val wordList = loadWordList(options('dictFile).asInstanceOf[String],
+      options('pointsMethod).asInstanceOf[Symbol])
+    val (bestSet, bestPoints) = BestSet.findBestSet(wordList, options('maxSize).asInstanceOf[Int])
+    val validWords = wordList
+      .filter(_.matchesLetterSet(bestSet))
+      .filter(_.points > 0)
     println("Best set: " + bestSet)
-    println("Word count: " + bestCount)
+    println("Point count: " + bestPoints)
+    println("Word count: " + validWords.size)
     if (options('showWords).asInstanceOf[Boolean]) {
-      val validWords = wordList.filter(_.matchesLetterSet(bestSet))
       println("Words:")
       println("====================")
       for (word <- validWords) println(word.spelling)
@@ -30,6 +38,7 @@ object WordScoreApp {
 
       argList match {
         case Nil => map
+        case "--boggle" :: tail => parseNextOption(map ++ Map('pointsMethod -> 'boggle), tail)
         case "--words" :: tail => parseNextOption(map ++ Map('showWords -> true), tail)
         case "--size" :: value :: tail => parseNextOption(map ++ Map('maxSize -> value.toInt), tail)
         case value :: flag :: tail if isSwitch(flag) =>
@@ -44,11 +53,11 @@ object WordScoreApp {
     parseNextOption(defaultOptions, argList)
   }
 
-  def loadWordList(filename: String): List[Word] = {
+  def loadWordList(filename: String, pointsMethod: Symbol = 'default): List[Word] = {
     val source = Source.fromFile(filename)
     val words = source.getLines.toList
     val validWords = words.filter(word => word.matches("[a-z]+"))
-    validWords.map(word => new Word(word))
+    validWords.map(word => new Word(word, pointsMethod))
   }
 
   def printWordList(wordList: List[Word]): Unit = {
