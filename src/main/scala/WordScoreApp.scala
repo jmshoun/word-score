d36d07e8bc5780eb5e8a6627a23fe75590cf1cc2
Mark Shoun <mark.shoun@gmail.com>
2018-02-01 21:21:00 -0500

    Create main entry point for application

---
+++ src/main/scala/WordScoreApp.scala
@@ -0,0 +1,47 @@
+import scala.io.Source
+
+object WordScoreApp {
+  type OptionMap = Map[Symbol, Any]
+  val defaultOptions: OptionMap = Map('maxSize -> 7)
+
+  def main(args: Array[String]) = {
+    val argList = args.toList
+    val options = parseOptions(argList)
+    if (!options.contains('dictFile)) {
+      println("Dictionary file must be supplied!")
+      System.exit(1)
+    }
+    val wordList = loadWordList(options('dictFile).asInstanceOf[String])
+    printWordList(wordList)
+  }
+
+  def parseOptions(argList: List[String]): OptionMap = {
+    def parseNextOption(map: OptionMap, argList: List[String]): OptionMap = {
+      def isSwitch(s: String) = s(0) == '-'
+
+      argList match {
+        case Nil => map
+        case "--size" :: value :: tail => parseNextOption(map ++ Map('maxSize -> value.toInt), tail)
+        case value :: flag :: tail if isSwitch(flag) =>
+          parseNextOption(map ++ Map('dictFile -> value), argList.tail)
+        case value :: Nil => map ++ Map('dictFile -> value)
+        case option :: tail =>
+          println("Unknown option " + option)
+          map
+      }
+    }
+
+    parseNextOption(defaultOptions, argList)
+  }
+
+  def loadWordList(filename: String): List[Word] = {
+    val source = Source.fromFile(filename)
+    val words = source.getLines.toList
+    val validWords = words.filter(word => word.matches("[a-z]+"))
+    validWords.map(word => new Word(word))
+  }
+
+  def printWordList(wordList: List[Word]): Unit = {
+    for (word <- wordList) println(word.spelling)
+  }
+}
--- src/test/scala/WordListTest.scala
+++ src/test/scala/WordListTest.scala
@@ -2,7 +2,7 @@ import org.scalatest.FlatSpec
 
 class WordListTest extends FlatSpec {
   "Word lists" should "scrub bad words" in {
-    val wordList = WordList.newList("src/test/resources/sample_dict.txt")
+    val wordList = WordScoreApp.loadWordList("src/test/resources/sample_dict.txt")
     assert(wordList.size == 3)
     assert(wordList(0).spelling == "ate")
     assert(wordList(1).spelling == "eat")
